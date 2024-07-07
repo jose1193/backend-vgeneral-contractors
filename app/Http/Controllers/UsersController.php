b990b72dd9ab0f940c17cb705f3043947561f874
jose1193 <josegonzalezcr2794@gmail.com>
2024-07-08 00:12:10 +0100

    first commit

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,191 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Http\Controllers\BaseController as BaseController;
+use Illuminate\Http\Request;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Response;
+use Illuminate\Http\JsonResponse;
+use App\Models\User;
+use Hash;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\QueryException;
+use Illuminate\Support\Facades\Log;
+use Ramsey\Uuid\Uuid;
+use Illuminate\Support\Facades\Cache;
+
+use App\Interfaces\UsersRepositoryInterface;
+use App\Http\Requests\CreateUserRequest;
+use App\Classes\ApiResponseClass;
+use App\Http\Resources\UserResource;
+
+use App\Services\UserService;
+
+class UsersController extends BaseController
+{
+
+    
+    protected $cacheTime = 720;
+   
+
+    protected $userService;
+
+    public function __construct(UserService $userService)
+    {
+         // Middleware para permisos
+        $this->middleware('check.permission:Super Master')->only(['index', 'create', 'store', 'edit', 'update', 'destroy']);
+        
+        $this->userService = $userService;
+    }
+
+    
+
+    
+    // SHOW LIST OF USERS
+
+       public function index(): JsonResponse
+    {
+        try {
+            // Obtener todos los usuarios usando el servicio
+            $users = $this->userService->all();
+
+            if ($users === null) {
+                return response()->json(['message' => 'No users found or invalid data structure'], 404);
+            }
+
+            return ApiResponseClass::sendResponse(UserResource::collection($users), 200);
+
+        } catch (QueryException $e) {
+            Log::error('Database error occurred while fetching users: ' . $e->getMessage(), [
+                'exception' => $e
+            ]);
+            return response()->json(['message' => 'Database error occurred while fetching users'], 500);
+
+        } catch (\Exception $e) {
+            Log::error('Error occurred while fetching users: ' . $e->getMessage(), [
+                'exception' => $e
+            ]);
+            return response()->json(['message' => 'Error occurred while fetching users'], 500);
+        }
+    }
+
+
+
+    // SYNC ROLES
+    public function create()
+{
+    $cacheKey = 'roles_list';
+    $roles = $this->getCachedData($cacheKey, 360, function () {
+        return Role::orderBy('id', 'DESC')->get();
+    });
+    return response()->json(['roles' => $roles], 200);
+}
+
+
+
+    // STORE USER
+   
+public function store(CreateUserRequest $request)
+{
+    try {
+        // Validar y obtener los detalles de la solicitud
+        $details = $request->validated();
+       
+        // Utilizar el servicio para almacenar el usuario
+        $user = $this->userService->storeUser($details);
+
+        return ApiResponseClass::sendSimpleResponse(new UserResource($user), 200);
+    } catch (\Exception $ex) {
+        return response()->json(['message' => 'Error occurred while creating user', 'error' => $ex->getMessage()], 500);
+    }
+}
+
+
+
+   // Método para actualizar un usuario
+   public function update(CreateUserRequest $request, $uuid): JsonResponse
+    {
+        $updateDetails = $request->validated();
+
+        try {
+           // Utilizar el servicio para actualizar el usuario
+            $user = $this->userService->updateUser($updateDetails, $uuid);
+
+            return ApiResponseClass::sendSimpleResponse(new UserResource($user), 200);
+
+        } catch (\Exception $ex) {
+            return response()->json(['message' => 'Error occurred while updating user', 'error' => $ex->getMessage()], 500);
+        }
+    }
+
+
+    // SHOW PROFILE USER
+   public function show($uuid)
+{
+    try {
+        // Utilizar el servicio para obtener el usuario
+        $user = $this->userService->showUser($uuid);
+
+        return ApiResponseClass::sendSimpleResponse(new UserResource($user), 200);
+
+    } catch (\Exception $e) {
+        // Registrar el mensaje de la excepción en el log
+        Log::error('Error occurred while fetching user: ' . $e->getMessage());
+
+        // Manejar cualquier excepción y devolver una respuesta de error
+        return response()->json(['message' => 'Error occurred while fetching user', 'error' => $e->getMessage()], 500);
+    }
+}
+
+
+
+    // USER DELETE
+
+ public function destroy($uuid)
+{
+    try {
+        // Utilizar el servicio para obtener el usuario
+        $user = $this->userService->deleteUser($uuid);
+
+        return ApiResponseClass::sendResponse('User Delete Successful','',200);
+
+    } catch (\Exception $e) {
+        // Registrar el mensaje de la excepción en el log
+        Log::error('Error occurred while deleting user: ' . $e->getMessage());
+
+        // Manejar cualquier excepción y devolver una respuesta de error
+        return response()->json(['message' => 'Error occurred while deleting user', 'error' => $e->getMessage()], 500);
+    }
+}
+
+   
+
+
+// USER RESTORE
+
+ public function restore($uuid)
+{
+    try {
+        // Utilizar el servicio para obtener el usuario
+        $user = $this->userService->restoreUser($uuid);
+
+            return ApiResponseClass::sendSimpleResponse(new UserResource($user), 200);
+
+    } catch (\Exception $e) {
+        // Registrar el mensaje de la excepción en el log
+        Log::error('Error occurred while restoring user: ' . $e->getMessage());
+
+        // Manejar cualquier excepción y devolver una respuesta de error
+        return response()->json(['message' => 'Error occurred while restoring user', 'error' => $e->getMessage()], 500);
+    }
+}
+
+
+
+
+
+}
---
+++ routes/api.php
@@ -0,0 +1,110 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UsersController;
+use App\Http\Controllers\PermissionController;
+//use App\Http\Controllers\Api\ErrorController;
+use Illuminate\Support\Facades\Response;
+use App\Http\Controllers\ProfilePhotoController;
+
+use App\Http\Controllers\CheckUsernameController;
+use App\Http\Controllers\CheckEmailController;
+use App\Http\Controllers\BiometricAuthController;
+use App\Http\Controllers\SocialLoginController;
+use App\Http\Controllers\CreateUserController;
+use App\Http\Controllers\PasswordResetUserController;
+
+//Route::get('/user', function (Request $request) {
+    //return $request->user();
+//})->middleware('auth:sanctum');
+
+Route::post('login', [AuthController::class, 'login']);
+
+Route::post('/register', [CreateUserController::class, 'store']);
+
+Route::get('/username-available/{username}', [CheckUsernameController::class, 'checkUsernameAvailability']);
+
+Route::get('/email-available/{email}', [CheckEmailController::class, 'checkEmailAvailability']);
+
+// Route related to User Social Login
+Route::post('/social-login', [SocialLoginController::class, 'handleProviderCallback']);
+
+
+Route::controller(PasswordResetUserController::class)->group(function () {
+    Route::post('/forgot-password', 'store'); 
+    Route::post('/enter-pin', 'verifyResetPassword');
+    Route::post('/reset-password', 'updatePassword');  
+   
+});
+
+
+
+
+//Route::controller(RegisterController::class)->group(function(){
+    //Route::post('register', 'register');
+    //Route::post('login', 'login');
+//});
+
+
+
+Route::middleware(['auth:sanctum','handle.notfound'])->group(function() {
+    //Route::get('/user', function (Request $request) {
+        //return $request->user();
+    //});
+
+    // Rutas protegidas por autenticación y verificación
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::get('user', [AuthController::class, 'user']);
+    //Route::get('/users', [AuthController::class, 'getUsers']);
+    Route::post('update-password', [AuthController::class, 'updatePassword']);
+    
+    //Route::post('reset-password', [AuthController::class, 'resetPassword']);
+    Route::post('update-profile', [CreateUserController::class, 'update']);
+    Route::post('update-profile-photo', [ProfilePhotoController::class, 'update']);
+    
+
+    // Rutas relacionadas con roles
+    Route::get('roles-list', [RoleController::class, 'index']); // Obtener una lista de roles
+    Route::post('roles', [RoleController::class, 'store']); // Crear un nuevo rol
+    Route::get('roles/{id}', [RoleController::class, 'show']); // Mostrar un rol específico
+    Route::put('roles-update/{id}', [RoleController::class, 'update']); // Actualizar un rol existente
+    Route::delete('roles-delete/{id}', [RoleController::class, 'destroy']); // Eliminar un rol existente
+    Route::get('roles-permissions', [RoleController::class, 'create']); // Mostrar listado de permisos
+    Route::get('roles/{id}/edit', [RoleController::class, 'edit']); // Mostrar listado de roles y permisos del usuario a editar
+
+    
+    // Routes related to Users
+    Route::prefix('users')->group(function () {
+    Route::get('/', [UsersController::class, 'index']);    
+    Route::post('/store', [UsersController::class, 'store']); 
+    Route::get('/{uuid}', [UsersController::class, 'show']); 
+    Route::put('/update/{uuid}', [UsersController::class, 'update']); 
+    Route::post('/update-images/{uuid}/', [UsersController::class, 'updateImage']); 
+    Route::delete('delete/{uuid}', [UsersController::class, 'destroy']);
+    Route::put('restore/{uuid}', [UsersController::class, 'restore']);  
+    
+    }); 
+
+    // Rutas relacionadas con permisos
+    Route::get('permissions-list', [PermissionController::class, 'index']);
+    Route::post('permissions', [PermissionController::class, 'store']);
+    Route::get('permissions/{id}', [PermissionController::class, 'show']);
+    Route::put('permissions-update/{id}', [PermissionController::class, 'update']);
+    Route::delete('permissions-delete/{id}', [PermissionController::class, 'destroy']);
+    Route::get('permissions/create', [PermissionController::class, 'create']);
+    Route::get('permissions/{id}/edit', [PermissionController::class, 'edit']);
+
+    
+   
+    // Routes related to Biometric Login
+    Route::post('/biometric-login', [BiometricAuthController::class, 'store']);
+
+   
+    // Otras rutas protegidas...
+});
+    
