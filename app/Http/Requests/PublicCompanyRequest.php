9ce282ae724fd729fc6d1727b3e31e9cbf8cf27d
jose1193 <josegonzalezcr2794@gmail.com>
2024-10-22 20:44:51 +0100

    FIX: Public Company Request

--- app/Http/Requests/PublicCompanyRequest.php
+++ app/Http/Requests/PublicCompanyRequest.php
@@ -31,7 +31,7 @@ class PublicCompanyRequest extends FormRequest
             'unit' => ['nullable', 'string', 'max:255'],
             'phone' => ['nullable', 'string', 'max:255'],
             'email' => ['nullable', 'string', 'email', 'max:255'],
-            'website' => ['nullable', 'string', 'url', 'max:255'],
+            'website' => ['nullable', 'string', 'max:255'],
         ];
 
         if ($this->isMethod('put') || $this->isMethod('patch')) {
